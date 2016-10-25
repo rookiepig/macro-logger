13198b37e95148cbd651059b73e68b259542e260
Zhang Kang <zhangkang-pd@360.cn>
2016-10-25 16:08:03 +0800

    support android platform
    -- use __android_log_print()
    -- force android log level to ANDROID_LOG_ERROR
    -- set android tag to "LOG"

--- macrologger.h
+++ macrologger.h
@@ -23,11 +23,15 @@
 #ifndef __MACROLOGGER_H__
 #define __MACROLOGGER_H__
 
-#ifdef __OBJC__
+#if defined __OBJC__
 #import <Foundation/Foundation.h>
-#else
-#include <time.h>
+#elif defined __ANDROID__
+#include <android/log.h>
 #include <string.h>
+#include <time.h>
+#else
+#include <string.h>
+#include <time.h>
 #endif
 
 // === auxiliar functions
@@ -35,90 +39,104 @@ static inline char *timenow();
 
 #define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
 
-#define NO_LOG          0x00
-#define ERROR_LEVEL     0x01
-#define INFO_LEVEL      0x02
-#define DEBUG_LEVEL     0x03
+#define NO_LOG 0x00
+#define ERROR_LEVEL 0x01
+#define INFO_LEVEL 0x02
+#define DEBUG_LEVEL 0x03
 
 #ifndef LOG_LEVEL
-#define LOG_LEVEL   DEBUG_LEVEL
+#define LOG_LEVEL DEBUG_LEVEL
 #endif
 
-#ifdef __OBJC__
+#if defined __OBJC__
 
 #if __has_feature(objc_arc)
-#define AUTORELEASEPOOL_BEGIN   @autoreleasepool {
-#define AUTORELEASEPOOL_END     }
-#define RELEASE(OBJ)            OBJ = nil
+#define AUTORELEASEPOOL_BEGIN @autoreleasepool {
+#define AUTORELEASEPOOL_END }
+#define RELEASE(OBJ) OBJ = nil
 #else
-#define AUTORELEASEPOOL_BEGIN   NSAutoreleasePool *_pool = [[NSAutoreleasePool alloc] init];
-#define AUTORELEASEPOOL_END     [_pool release];
-#define RELEASE(OBJ)            [OBJ release];
+#define AUTORELEASEPOOL_BEGIN \
+  NSAutoreleasePool *_pool = [[NSAutoreleasePool alloc] init];
+#define AUTORELEASEPOOL_END [_pool release];
+#define RELEASE(OBJ) [OBJ release];
 #endif
 
-#define PRINTFUNCTION(format, ...)      objc_print(@format, __VA_ARGS__)
+#define PRINTFUNCTION(format, ...) objc_print(@format, __VA_ARGS__)
+
+#elif defined __ANDROID__
+
+// adding support for android platform
+#define PRINTFUNCTION(format, ...) \
+  __android_log_print(ANDROID_LOG_ERROR, "LOG", format, __VA_ARGS__)
+
 #else
-#define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
+
+#define PRINTFUNCTION(format, ...) fprintf(stderr, format, __VA_ARGS__)
 
 #endif
 
-#define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
-#define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
+#define LOG_FMT "%s | %-7s | %-15s | %s:%d | "
+#define LOG_ARGS(LOG_TAG) timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
 
-#define NEWLINE     "\n"
+#define NEWLINE "\n"
 
-#define ERROR_TAG   "ERROR"
-#define INFO_TAG    "INFO"
-#define DEBUG_TAG   "DEBUG"
+#define ERROR_TAG "ERROR"
+#define INFO_TAG "INFO"
+#define DEBUG_TAG "DEBUG"
 
 #if LOG_LEVEL >= DEBUG_LEVEL
-#define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
+#define LOG_DEBUG(message, args...) \
+  PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ##args)
 #else
 #define LOG_DEBUG(message, args...)
 #endif
 
 #if LOG_LEVEL >= INFO_LEVEL
-#define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
+#define LOG_INFO(message, args...) \
+  PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ##args)
 #else
 #define LOG_INFO(message, args...)
 #endif
 
 #if LOG_LEVEL >= ERROR_LEVEL
-#define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#define LOG_ERROR(message, args...) \
+  PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ##args)
 #else
 #define LOG_ERROR(message, args...)
 #endif
 
 #if LOG_LEVEL >= NO_LOGS
-#define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#define LOG_IF_ERROR(condition, message, args...) \
+  if (condition)                                  \
+  PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ##args)
 #else
 #define LOG_IF_ERROR(condition, message, args...)
 #endif
 
 static inline char *timenow() {
-    static char buffer[64];
-    time_t rawtime;
-    struct tm *timeinfo;
-    
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    
-    strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);
-    
-    return buffer;
+  static char buffer[64];
+  time_t rawtime;
+  struct tm *timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);
+
+  return buffer;
 }
 
 #ifdef __OBJC__
 
 static inline void objc_print(NSString *format, ...) {
-    AUTORELEASEPOOL_BEGIN
-    va_list args;
-    va_start(args, format);
-    NSString *logStr = [[NSString alloc] initWithFormat:format arguments:args];
-    fprintf(stderr, "%s", [logStr UTF8String]);
-    RELEASE(logStr);
-    va_end(args);
-    AUTORELEASEPOOL_END
+  AUTORELEASEPOOL_BEGIN
+  va_list args;
+  va_start(args, format);
+  NSString *logStr = [[NSString alloc] initWithFormat:format arguments:args];
+  fprintf(stderr, "%s", [logStr UTF8String]);
+  RELEASE(logStr);
+  va_end(args);
+  AUTORELEASEPOOL_END
 }
 
 #endif
